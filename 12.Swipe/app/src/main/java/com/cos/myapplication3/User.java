38bf6658455d7256c440587cdf6e49d30cfbc452
irone <doyeck@naver.com>
2021-02-18 15:05:07 +0900

    12. Swipe 로 이벤트주기

---
+++ 12.Swipe/app/src/main/java/com/cos/myapplication3/User.java
@@ -0,0 +1,15 @@
+package com.cos.myapplication3;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    private Integer id;
+    private String username;
+}
